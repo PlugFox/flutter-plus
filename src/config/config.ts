4d67631d50dda4fc297fd383fb2522f2df0564a3
Michael Lazebny <michael@lazebny.io>
2024-08-22 20:16:50 +0200

    [WIP]: Custom wrap with

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+import { workspace } from "vscode";
+
+export type CustomWrapConfig = {
+  name: string,
+  id: string,
+  body: Array<string>,
+};
+
+export class FlutterPlusConfig {
+  private static instance: FlutterPlusConfig;
+
+  public static getInstance(): FlutterPlusConfig {
+    if (!FlutterPlusConfig.instance) {
+      FlutterPlusConfig.instance = new FlutterPlusConfig();
+    }
+    return FlutterPlusConfig.instance;
+  }
+
+  public getCustomWraps(): Array<CustomWrapConfig> {
+    const config = workspace.getConfiguration('flutterplus');
+    const customWraps = config.get<Array<CustomWrapConfig>>('customWraps');
+
+    return customWraps || [];
+  }
+}
