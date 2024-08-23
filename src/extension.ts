01bfde1f5321e4d4fff3728923f509d614031e4f
Plague Fox <plugfox@gmail.com>
2024-08-23 15:28:48 +0400

    feat: Add support for Flutter Plus extension activation and SDK commands
    This commit adds support for activating the Flutter Plus extension and registering SDK commands. It ensures that the required Dart and Flutter extensions are installed and activated before proceeding. If any of the extensions are missing or fail to activate, an error message is displayed.
    
    The SDK commands are registered using the `registerCommands` function, which is called during extension activation. The `registerCommands` function registers the `flutter-plus.sealed-states` command.
    
    Additionally, the commit registers wrappers for the "Wrap with..." functionality using the `registerWrappers` function. The wrappers registered include `flutter-plus.wrap-sizedbox`, `flutter-plus.wrap-listenablebuilder`, `flutter-plus.wrap-valuelistenablebuilder`, and `flutter-plus.wrap-repaintboundary`.
    
    This commit also introduces a new `SdkCommands` class in the `src/utils/sdk.ts` file. The `SdkCommands` class handles the registration and execution of SDK commands. It takes the `dartExtensionApi` as a parameter and uses it to run the desired function if it is supported. If the function is not available, an error message is displayed.
    
    The changes in this commit aim to enhance the functionality and usability of the Flutter Plus extension by adding support for activation and SDK commands.

--- src/extension.ts
+++ src/extension.ts
@@ -17,21 +17,64 @@ import {
 	CodeActionWrap,
 } from './code-actions';
 
+
+import {
+	dartCodeExtensionIdentifier,
+	flutterExtensionIdentifier,
+} from "./constants";
+import { SdkCommands } from './utils/sdk';
+
 const DART_MODE = { language: "dart", scheme: "file" };
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+	// Ensure we have a Dart extension.
+	const dartExt = vscode.extensions.getExtension(dartCodeExtensionIdentifier);
+	if (!dartExt) {
+		// This should not happen since the Flutter extension has a dependency on the Dart one
+		// but just in case, we'd like to give a useful error message.
+		throw new Error("The Dart extension is not installed, Flutter extension is unable to activate.");
+	}
+	await dartExt.activate();
+	if (!dartExt.exports) {
+		console.error("The Dart extension did not provide an exported API. Maybe it failed to activate or is not the latest version?");
+		return;
+	}
+
+	// Ensure we have a Flutter extension.
+	const flutterExt = vscode.extensions.getExtension(flutterExtensionIdentifier);
+	if (!flutterExt) {
+		// This should not happen since the Flutter extension has a dependency on the Dart one
+		// but just in case, we'd like to give a useful error message.
+		throw new Error("The Flutter extension is not installed, Flutter Plus extension is unable to activate.");
+	}
+	await flutterExt.activate();
+
+	// Register SDK commands.
+	const sdkCommands = new SdkCommands(context, dartExt.exports);
+
 	//console.log('Congratulations, your extension "flutter-plus" is now active!');
 
+	registerCommands(context);
+	registerWrappers(context);
+}
+
+/// Register all commands.
+function registerCommands(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		/* vscode.commands.registerCommand('flutter-plus.helloWorld', () => {
 			vscode.window.showInformationMessage('Hello World from Flutter Plus!');
 		}), */
 		commands.registerCommand("flutter-plus.sealed-states", sealedStates),
+	);
+}
+
+/// Register all wrappers (Wrap With...).
+function registerWrappers(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
 		commands.registerCommand("flutter-plus.wrap-sizedbox", wrapWithSizedBox),
 		commands.registerCommand("flutter-plus.wrap-listenablebuilder", wrapWithListenableBuilder),
 		commands.registerCommand("flutter-plus.wrap-valuelistenablebuilder", wrapWithValueListenableBuilder),
 		commands.registerCommand("flutter-plus.wrap-repaintboundary", wrapWithRepaintBoundary),
-
 		languages.registerCodeActionsProvider(
 			DART_MODE,
 			new CodeActionWrap(),
